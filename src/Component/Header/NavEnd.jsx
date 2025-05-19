d4f71dcd98d268389f5633b372f81de932022ac7
asif883 <asifiqbal95863@gmail.com>
2025-05-19 18:01:54 +0600

    update navEnd

--- src/Component/Header/NavEnd.jsx
+++ src/Component/Header/NavEnd.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import { FaSearch } from "react-icons/fa";
 import { TiShoppingCart } from "react-icons/ti";
+import { FaSearch } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import useUserData from '../../Hooks/useUserData';
+import { MdOutlineDashboard } from 'react-icons/md';
+
+
 const NavEnd = () => {
+    const {role } = useUserData()
+
     return (
         <div className='flex gap-4 items-center'>
             <div className=''>
-                <button className='cursor-pointer'><FaSearch size={24}/></button>
+                <Link to='#'><FaSearch size={27}/></Link>
             </div>
-            <div className=''>        
-               <div className="indicator">
-                  <span className="indicator-item bg-gray-text-gray-400  badge rounded-badge badge-sm text-black px-1">0</span>
-                    <Link to='/dashboard/my-cart' className=""><TiShoppingCart size={24}/></Link>
+            <div className='flex items-center'>        
+               {
+                 role === 'admin' ? 
+                 <Link to='/dashboard/overview'>
+                    <MdOutlineDashboard size={30}/>
+                </Link>
+                :
+                <div>
+                    <Link to='/dashboard/my-cart' className=""><TiShoppingCart size={28}/></Link>
                 </div>
+               }
             </div>
         </div>
     );
